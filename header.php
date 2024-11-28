56506d4e6a85c26edfe32c62ddcedc4242e8ecf1
Amarpal Singh Matharu <singh14amarpal2004@gmail.com>
2024-11-28 14:24:39 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,50 @@
+<section class="header header-sticky">
+<nav class="navbar navbar-expand-lg bg-body-tertiary ">
+  <div class="container">
+    <a class="navbar-brand" href="#"><img src="<?php echo BASE_URL; ?>/images/logo.png" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse  justify-content-end" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active"  href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Men's</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Women's</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Kids's</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Pages
+          </a>
+          <ul class="dropdown-menu m-0 p-0">
+            <li><a class="dropdown-item" href="javascript:void(0);">About Us</a></li>
+            <li><a class="dropdown-item" href="#">Products</a></li>
+            <li><a class="dropdown-item" href="#">Single Product</a></li>
+            <li><a class="dropdown-item" href="#">Contact Us</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Features
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link m-0" href="#">Explore</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</section>
